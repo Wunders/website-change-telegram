2921696f9d2525532dcbdd4240a7dad7e0e78b11
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-01-13 11:20:10 +0530

    Update website-change-telegram.py

--- website-change-telegram.py
+++ website-change-telegram.py
@@ -12,8 +12,8 @@ from lxml import html
 
 # Send a message via a telegram bot
 def telegram_bot_sendtext(bot_message):
-    bot_token = '1675779827:AAE9YVNAjz4A0LiA0TDaewTvYLLfWn9_HTo'
-    bot_chatID = '744730993'
+    bot_token = ' '
+    bot_chatID = ' '
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
